daa48c7f8a36d57e59d64844d4180aa5916f5e39
SuoRui <suorui.cn@live.cn>
2023-01-16 01:10:13 +0800

    支持创建磁盘接口

--- src/disk.rs
+++ src/disk.rs
@@ -3,3 +3,40 @@ mod hdd;
 pub fn init() -> Result<(), &'static str> {
     Ok(())
 }
+
+pub enum DisKType {
+    HDD,
+    SSD,
+}
+
+pub fn create_new_disk(disk_type: DisKType) -> Result<(), &'static str> {
+    match disk_type {
+        DisKType::HDD => {
+            println!("Start create a HDD disk");
+            Ok(())
+        }
+        DisKType::SSD => {
+            println!("Start create a SSD disk");
+            Ok(())
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_hdd() {
+        if let Err(e) = create_new_disk(super::DisKType::HDD) {
+            assert!(false, "{}", e);
+        }
+    }
+
+    #[test]
+    fn test_create_ssd() {
+        if let Err(e) = create_new_disk(super::DisKType::SSD) {
+            assert!(false, "{}", e);
+        }
+    }
+}
